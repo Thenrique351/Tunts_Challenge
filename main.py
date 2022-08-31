1393f11554ed653a914811ff705b577ceadf55ce
Tiago Rosa <thenrique351@gmail.com>
2022-08-31 13:07:53 -0300

    Request URL module create

---
+++ main.py
@@ -0,0 +1,5 @@
+from request import request_url
+
+
+r = request_url('https://restcountries.com/v3/all')# address of API
+print(r)
---
+++ request.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+def request_url(url):
+    request = requests.get(url)
+    data = request.json()#Json open 
+
+    resp = []
+
+    for i in data:
+        x = i.get('name')
+        country = x.get('common','-')
+        
+        #print(y)
+        x = i.get('capital','-')
+        capital = x[0]
+        #print(y)
+        area = i.get('area','-')
+        #print(x)
+        x = i.get('currencies','-')
+        if(x != '-'):
+            #currencies = str(list(x.keys())).strip('[]')
+            currencies = ', '.join((list(x.keys())))#convert the dict type in list type
+        else:
+            currencies = '-' 
+        dicionario = {'Name': country, 'Capital': capital, 'Area': area, 'Currencies': currencies}
+        resp.append(dicionario)#Add object in list, on format {Name, Capital, Area, Currencies}
+    
+    return resp #Return  Json objects by name, capital, area and currencies in list of  dictionary python format
